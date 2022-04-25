d45496b351c96d8b593d3108a1a6f2fd064814db
gokcaneren <gokcann01@gmail.com>
2022-04-25 13:28:36 +0300

    Codefirst added!

---
+++ EFCoreDatabaseFirst/EFCoreDatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreDatabaseFirst.DAL
+{
+    internal class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-I78CKSO;Initial Catalog=UdemyEFCoreDatabaseFirstDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
