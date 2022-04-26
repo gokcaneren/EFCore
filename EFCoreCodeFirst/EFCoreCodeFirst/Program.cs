d773697b0e483cf4abdb367bd570c88a5b715d0b
gokcaneren <gokcann01@gmail.com>
2022-04-26 13:19:42 +0300

    Migration Used!

---
+++ EFCoreCodeFirst/EFCoreCodeFirst/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using EFCoreCodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+
+using (var _context=new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id}:{p.Name} - {p.Price} - {p.Stock}");
+    });
+}
+
