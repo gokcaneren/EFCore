dbab87d6a3d37b8292efffd08be0f64c62c6adc8
gokcaneren <gokcann01@gmail.com>
2022-04-26 13:41:15 +0300

    add-migration/update-database learned!

--- EFCoreCodeFirst/EFCoreCodeFirst/DAL/Product.cs
+++ EFCoreCodeFirst/EFCoreCodeFirst/DAL/Product.cs
@@ -15,5 +15,7 @@ namespace EFCoreCodeFirst.DAL
         public decimal Price { get; set; }
 
         public int Stock { get; set; }
+
+        public int Barcode { get; set; }
     }
 }
--- EFCoreCodeFirst/EFCoreCodeFirst/Migrations/20220426102434_AddBarcode_ForProducts.Designer.cs
+++ EFCoreCodeFirst/EFCoreCodeFirst/Migrations/20220426102434_AddBarcode_ForProducts.Designer.cs
@@ -3,6 +3,7 @@ using EFCoreCodeFirst.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFCoreCodeFirst.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220426102434_AddBarcode_ForProducts")]
+    partial class AddBarcode_ForProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -29,8 +31,9 @@ namespace EFCoreCodeFirst.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
 
-                    b.Property<int>("Barcode")
-                        .HasColumnType("int");
+                    b.Property<string>("Barcode")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Name")
                         .IsRequired()
