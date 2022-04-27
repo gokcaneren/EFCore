74d429a14074b3d5b293063f0aaaf668aec62630
gokcaneren <gokcann01@gmail.com>
2022-04-27 13:41:14 +0300

    Learning states

--- EFCoreCodeFirst/EFCoreCodeFirst/Program.cs
+++ EFCoreCodeFirst/EFCoreCodeFirst/Program.cs
@@ -8,11 +8,45 @@ Initializer.Build();
 
 using (var _context=new AppDbContext())
 {
-    var products = await _context.Products.ToListAsync();
 
-    products.ForEach(p =>
+    //var newProduct = new Product
+    //{
+    //    Name = "Kalem 33",
+    //    Price = 200,
+    //    Stock=23,
+    //    Barcode=333
+    //};
+
+    //var product = await _context.Products.FirstAsync();
+    //Console.WriteLine($"First State:{_context.Entry(product).State}");
+    //_context.Entry(product).State = EntityState.Detached;
+    //Console.WriteLine($"Last State:{_context.Entry(product).State}");
+    //_context.Entry(newProduct).State = EntityState.Added;
+    //product.Stock = 1000;
+    //_context.Remove(product);
+    //await _context.AddAsync(newProduct);
+    //product.Name = "Kaleeeeeeeeeemmmmm";
+
+    //Console.WriteLine($"Last State:{_context.Entry(product).State}");
+
+    _context.Update(new Product()
     {
-        Console.WriteLine($"{p.Id}:{p.Name} - {p.Price} - {p.Stock}");
+        Id=3,
+        Name="Defter",
     });
+
+    await _context.SaveChangesAsync();
+    //Console.WriteLine($"After save changes:{_context.Entry(product).State}");
+
+
+    //var products = await _context.Products.ToListAsync();
+
+    //products.ForEach(p =>
+    //{
+    //    var state = _context.Entry(p).State;
+
+
+    //    Console.WriteLine($"{p.Id}:{p.Name} - {p.Price} - {p.Stock} -- {state}");
+    //});
 }
 
